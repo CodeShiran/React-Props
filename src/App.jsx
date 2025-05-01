15bebb8c47b635f06134c8e81085791a47475422
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-05-01 22:22:15 +0530

    food component completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Food from './components/Food/Food'
+
+import craft1 from './assets/craft1.jpg'
+import craft2 from './assets/craft2.jpg'
+import burger from './assets/burger.jpg'
+import chicken from './assets/chicken.jpg'
+import herobg from './assets/hero-bg.jpg'
+
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Food bgImg1 ={craft1} bgImg2 = {craft2} />
+      <Food bgImg={chicken} />
+      <Food bgImg={burger} />
     </div>
   )
 }
